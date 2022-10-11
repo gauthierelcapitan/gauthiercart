6cb7feda7fd90911aa2e2394ba4069766a28d9e0
gauthier <gauthier@livetrail.net>
2022-10-11 11:45:50 +0200

    chore: init devops env and front

---
+++ front/pages/index.tsx
@@ -0,0 +1,26 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Gauthier Cart</title>
+        <meta name="description" content="Gauthier Cart" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1 className="text-3xl font-bold underline">
+          Under construction
+        </h1>
+      </main>
+
+      <footer>
+
+      </footer>
+    </div>
+  )
+}
+
+export default Home
