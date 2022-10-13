b5fae36be88c12d9b3e3736ce66741d2f675660f
gauthier <gauthier@livetrail.net>
2022-10-13 09:05:41 +0200

    feat: configure linter

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -1,15 +1,15 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import dynamic from 'next/dynamic'
-import mountains from '../public/mont-blanc.jpg'
+import type { NextPage } from 'next';
+import dynamic from 'next/dynamic';
+import Head from 'next/head';
+import Image from 'next/image';
+
+import mountains from '../public/mont-blanc.jpg';
 
 const DynamicFog = dynamic(() => import('../components/fog'), {
-    ssr: false,
-})
+  ssr: false,
+});
 
 const Home: NextPage = () => {
-
   return (
     <div>
       <Head>
@@ -18,36 +18,33 @@ const Home: NextPage = () => {
       </Head>
 
       <main>
-          <div className="bg-gradient-to-tr from-rose-500 to-california-400 relative h-screen w-screen">
-              <Image
-                  className="absolute inset-0 w-full h-full object-cover mix-blend-multiply filter brightness-50"
-                  layout="fill"
-                  objectFit="cover"
-                  quality={100}
-                  src={mountains}
-                  alt="Toto"
-                  placeholder="blur"
-              />
-              <DynamicFog />
-              <div className="absolute inset-0 flex flex-col justify-center items-center w-5/6 max-w-lg mx-auto text-center">
-                   <h1 className="font-primary font-sans font-bold text-white text-3xl sm:text-4xl md:text-5xl md:leading-snug">
-                       Gauthier Cart
-                   </h1>
-                   <div className="space-y-8">
-
-                   <p className="font-secondary text-palette-light text-base md:text-lg lg:text-xl">
-                       Website under construction.
-                   </p>
-                   </div>
-              </div>
+        <div className="bg-gradient-to-tr from-rose-500 to-california-400 relative h-screen w-screen">
+          <Image
+            className="absolute inset-0 w-full h-full object-cover mix-blend-multiply filter brightness-50"
+            layout="fill"
+            objectFit="cover"
+            quality={100}
+            src={mountains}
+            alt="Toto"
+            placeholder="blur"
+          />
+          <DynamicFog />
+          <div className="absolute inset-0 flex flex-col justify-center items-center w-5/6 max-w-lg mx-auto text-center">
+            <h1 className="font-primary font-sans font-bold text-white text-3xl sm:text-4xl md:text-5xl md:leading-snug">
+              Gauthier Cart
+            </h1>
+            <div className="space-y-8">
+              <p className="font-secondary text-palette-light text-base md:text-lg lg:text-xl">
+                Website under construction.
+              </p>
+            </div>
           </div>
+        </div>
       </main>
 
-      <footer>
-
-      </footer>
+      <footer></footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
